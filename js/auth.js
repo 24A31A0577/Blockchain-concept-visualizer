eecdc5f233ff2ecd1488ffa6a61d55f02f7f2d98
24A31A0577 <bhanusaiguggilapu78@gmail.com>
2026-04-03 16:15:57 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -58,3 +58,11 @@ function logoutUser() {
     localStorage.removeItem("loggedInUser");
     window.location.href = "login.html";
 }
+function togglePassword() {
+    let pwd = document.getElementById("password");
+    if(pwd.type === "password") {
+        pwd.type = "text";
+    } else {
+        pwd.type = "password";
+    }
+}
